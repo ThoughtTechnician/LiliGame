2eb185ba78c7f25a7fc8d57d685718a059ec818b
Connor Reeder <connor.reeder@ymail.com>
2017-07-28 22:32:33 -0400

    Lili successfully running as an imageview

--- app/src/main/java/com/wordpress/simpledevelopments/liligame/GameActivity.java
+++ app/src/main/java/com/wordpress/simpledevelopments/liligame/GameActivity.java
@@ -1,13 +1,23 @@
 package com.wordpress.simpledevelopments.liligame;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.lili_layout);
+
+        ImageView liliView = (ImageView) findViewById(R.id.imageView);
+        liliView.setBackgroundResource(R.drawable.running_animation);
+
+
+        AnimationDrawable frameAnimation = (AnimationDrawable) liliView.getBackground();
+
+        frameAnimation.start();
     }
 }
